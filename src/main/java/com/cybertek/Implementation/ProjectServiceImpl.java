253cb3095187461a4d1ab6557a87abbcbca841cb
Mehmet <mhmtsytrk@gmail.com>
2021-01-07 03:27:23 +0300

    Ticketing Project - ORM Project Controller

--- src/main/java/com/cybertek/Implementation/ProjectServiceImpl.java
+++ src/main/java/com/cybertek/Implementation/ProjectServiceImpl.java
@@ -2,14 +2,17 @@ package com.cybertek.implementation;
 
 import com.cybertek.dto.ProjectDTO;
 import com.cybertek.entity.Project;
+import com.cybertek.entity.User;
 import com.cybertek.enums.Status;
 import com.cybertek.mapper.ProjectMapper;
 import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.ProjectRepository;
 import com.cybertek.service.ProjectService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
@@ -31,7 +34,10 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public List<ProjectDTO> listAllProjects() {
-        return null;
+
+        List<Project> list = projectRepository.findAll(Sort.by("projectName"));
+        return list.stream().map(obj ->{return projectMapper.convertToDto(obj);}).collect(Collectors.toList());
+
     }
 
     @Override
@@ -39,7 +45,9 @@ public class ProjectServiceImpl implements ProjectService {
         dto.setProjectStatus(Status.OPEN);
         Project obj = projectMapper.convertToEntity(dto);
         obj.setAssignedManager(userMapper.convertToEntity(dto.getAssignedManager()));
+
         projectRepository.save(obj);
+
     }
 
     @Override
