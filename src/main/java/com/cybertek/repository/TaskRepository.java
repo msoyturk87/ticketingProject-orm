4203d08922fb5a698188e50e9920c652fdf6dc93
Mehmet <mhmtsytrk@gmail.com>
2021-01-07 04:30:22 +0300

    Ticketing Project - ORM Task Controller is started

--- src/main/java/com/cybertek/repository/TaskRepository.java
+++ src/main/java/com/cybertek/repository/TaskRepository.java
@@ -1,4 +1,8 @@
 package com.cybertek.repository;
 
-public interface TaskRepository {
+import com.cybertek.entity.Role;
+import com.cybertek.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
 }
